1fc9fea1c3eda8cb16da901a603796ffcdefb507
juyeongsung <jooyongs1@gmail.com>
2022-05-01 12:35:12 +0900

    node 시작

---
+++ func.js
@@ -0,0 +1,8 @@
+const {odd, even} = require('./var')
+
+
+console.log(odd)
+console.log(even)
+
+console.log(__filename)
+console.log(__dirname)
---
+++ hello.js
@@ -0,0 +1,10 @@
+function sayHello() {
+  console.log('hello world')
+  node()
+}
+
+function node() {
+  console.log('node node')
+}
+
+sayHello()
